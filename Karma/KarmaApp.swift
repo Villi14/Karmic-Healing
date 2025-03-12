d74fb9e82a476f27ae40a3dbd20d4d50cc07fb71
Oleksandr Velykotskyi <avelikotckiy@gmail.com>
2025-03-12 11:54:23 +0200

    Initial Commit

---
+++ Karma/KarmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KarmaApp.swift
+//  Karma
+//
+//  Created by Oleksandr Velykotskyi on 12.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct KarmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
