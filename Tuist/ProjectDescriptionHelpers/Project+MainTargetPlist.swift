e337767a3f0a43c74b95658ccb0991c21a8da602
Oleksandr Velykotskyi <avelikotckiy@gmail.com>
2025-10-13 17:48:01 +0300

    v 0.6 b 0.7

--- Tuist/ProjectDescriptionHelpers/Project+MainTargetPlist.swift
+++ Tuist/ProjectDescriptionHelpers/Project+MainTargetPlist.swift
@@ -2,7 +2,7 @@ import Foundation
 @preconcurrency import ProjectDescription
 
 public let bundleShortVersionString: String = "1.2"
-public let bundleVersion: String = "0.6"
+public let bundleVersion: String = "0.7"
 
 public let mainTargetPlist: InfoPlist = .extendingDefault(
   with: [
